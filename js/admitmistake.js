db1540dd21800b1286c5356444f5ddcf0f52459c
吴煜龙 <wuyulong@wuyulongdeMacBook-Pro.local>
2020-02-05 23:39:28 +0800

    html basic

---
+++ js/admitmistake.js
@@ -0,0 +1,16 @@
+var btn = document.getElementById("btn");
+btn.addEventListener('click', function(){
+    alert("既然点了要不多点几下");
+    alert("错了错了");
+    alert("不忙不忙怎么会忙呢");
+    alert("不嫌弃怎么会嫌弃呢");
+    alert("没有吵到没有吵到怎么会嫌你吵呢");
+    alert("知道错了错了");
+    alert("不要不开心了");
+    alert("别关下面还有呢");
+    alert("小徒弟师傅知道错了");
+    alert("嗯？原谅师傅呗");
+    alert("你说呢？嗯？")
+    alert("拜托拜托");
+    alert("错了错了知道错了哦")
+})
